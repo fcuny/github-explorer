c55a169ead9beb27436144923255c254d215158f
franck cuny <franck@lumberjaph.net>
2010-02-10 11:07:41 +0100

    update to create graph

--- lib/githubexplorer/Schema/Result/Fork.pm
+++ lib/githubexplorer/Schema/Result/Fork.pm
@@ -6,23 +6,16 @@ __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('fork');
 
 __PACKAGE__->add_columns(
-    profile_origin => { data_type => 'int', },
-    profile_dest   => { data_type => 'int' },
-    repo_origin    => { data_type => 'int' },
-    repo_dest      => { data_type => 'int' },
+    profile => { data_type => 'int', },
+    repos   => { data_type => 'int' },
 );
 
-__PACKAGE__->set_primary_key(
-    qw/repo_origin repo_dest profile_origin profile_dest/ );
+__PACKAGE__->set_primary_key(qw/repos profile/);
 
-__PACKAGE__->belongs_to( 'profile_origin',
-    'githubexplorer::Schema::Result::Profiles' );
-__PACKAGE__->belongs_to( 'profile_dest',
+__PACKAGE__->belongs_to( 'profile',
     'githubexplorer::Schema::Result::Profiles' );
 
-__PACKAGE__->belongs_to( 'repo_origin',
-    'githubexplorer::Schema::Result::Repositories' );
-__PACKAGE__->belongs_to( 'repo_dest',
+__PACKAGE__->belongs_to( 'repos',
     'githubexplorer::Schema::Result::Repositories' );
 
 1;
